97cc4d86e66822df09d15a2487abf17f4550d6d6
Laphii <91127087+Laphii1206@users.noreply.github.com>
2026-06-01 22:44:21 +0800

    short to 3 month only

--- src/commands/rescheduleHandler.ts
+++ src/commands/rescheduleHandler.ts
@@ -148,18 +148,9 @@ export const data = new SlashCommandBuilder()
             .setDescription("Month (e.g. 08)")
             .setRequired(true)
             .addChoices([
-                { name: "Jan", value: "01" },
-                { name: "Feb", value: "02" },
-                { name: "Mar", value: "03" },
-                { name: "Apr", value: "04" },
-                { name: "May", value: "05" },
                 { name: "Jun", value: "06" },
                 { name: "Jul", value: "07" },
-                { name: "Aug", value: "08" },
-                { name: "Sep", value: "09" },
-                { name: "Oct", value: "10" },
-                { name: "Nov", value: "11" },
-                { name: "Dec", value: "12" }
+                { name: "Aug", value: "08" }
             ])
     )
     .addStringOption((opt: any) =>
