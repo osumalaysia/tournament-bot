f5a7e78f8828e0109c6483141261e0d75cc9cbfc
Laphii <91127087+Laphii1206@users.noreply.github.com>
2026-06-14 22:31:48 +0800

    add forfeit commands

---
+++ src/commands/matchSurrender.ts
@@ -0,0 +1,96 @@
+const { EmbedBuilder, SlashCommandBuilder } = require('discord.js');
+
+const TOURNAMENT_NAME = "o!M4T 2026";
+const EMOJI_GUILD_ID = "905398607895752735";
+const TARGET_CHANNEL_ID = "1457807376546533386";
+const ROLE_ID = "1457806106058297518";
+
+async function makeEmoji(emojiGuild: any, userId: any, name = "profile") {
+    try {
+        const res = await fetch(`https://a.ppy.sh/${userId}`);
+        const buf = Buffer.from(await res.arrayBuffer());
+        return await emojiGuild.emojis.create({ attachment: buf, name });
+    } catch {
+        return null;
+    }
+}
+
+export const data = new SlashCommandBuilder()
+    .setName("forfeit")
+    .setDescription("output result")
+    .addStringOption((matchid: any) =>
+        matchid.setName("matchid").setDescription("Match ID from the schedule sheet tab").setRequired(true)
+    )
+    .addStringOption((stages: any) =>
+        stages.setName("stages").setDescription("Stages of the match").setRequired(true)
+    )
+    .addStringOption((forfeit: any) =>
+        forfeit.setName("forfeitPlayer").setDescription("Player 1").setRequired(true)
+    )
+    .addStringOption((winner: any) =>
+        winner.setName("winnerPlayer").setDescription("Player 2").setRequired(true)
+    )
+    .addStringOption((forfeitSeed: any) =>
+        forfeitSeed.setName("player1seed").setDescription("Player 1 Seed").setRequired(true)
+    )
+    .addStringOption((winnerSeed: any) =>
+        winnerSeed.setName("player2seed").setDescription("Player 2 Seed").setRequired(true)
+    )
+    .addStringOption((forfeitId: any) =>
+        forfeitId.setName("forfeitId").setDescription("Player 1 ID").setRequired(true)
+    )
+    .addStringOption((winnerId: any) =>
+        winnerId.setName("winnerId").setDescription("Player 2 ID").setRequired(true)
+    )
+    .addStringOption((reason: any) =>
+        reason.setName("reason").setDescription("Reason for the forfeit").setRequired(false)
+    );
+
+export async function execute(interaction: any) {
+
+    if (!interaction.member.roles.cache.has(ROLE_ID)) {
+        await interaction.reply({ content: "Don't be an asshole", flags: 1 << 6 });
+        return;
+    } else if (!interaction.inGuild()) {
+        await interaction.reply({ content: "Thoughts you can use it in a dm huh!" });
+        return;
+    }
+
+    const emojiGuild = interaction.client.guilds.cache.get(EMOJI_GUILD_ID)
+    const channel = interaction.client.channels.cache.get(TARGET_CHANNEL_ID)
+
+    const iconURL = interaction.guild?.iconURL({ size: 512, extension: "png" }) || undefined;
+    const [forfeitProfile, winnerProfile] = await Promise.all([
+        makeEmoji(emojiGuild, interaction.options.getString("forfeitId"), "p1"),
+        makeEmoji(emojiGuild, interaction.options.getString("winnerId"), "p2"),
+    ]);
+
+    const data = {
+        matchId: interaction.options.getString("matchid"),
+        stages: interaction.options.getString("stages"),
+        forfeit: interaction.options.getString("forfeitPlayer"),
+        winner: interaction.options.getString("winnerPlayer"),
+        forfeitSeed: interaction.options.getString("player1seed"),
+        winnerSeed: interaction.options.getString("player2seed"),
+        forfeitId: interaction.options.getString("forfeitId"),
+        winnerId: interaction.options.getString("winnerId"),
+        reason: interaction.options.getString("reason") || "No reason provided",
+    };
+
+    const resultEmbed = new EmbedBuilder()
+        .setAuthor({ name: `${data.stages}: Match ${data.matchId}` })
+        .setTitle(`Win by Default for ${data.winner} ${data.winnerSeed} ${winnerProfile || ""}`)
+        .setDescription(
+            `**${data.forfeit}** ${data.forfeitSeed} ${forfeitProfile || ""} has forfeited the match.`
+        )
+        .setFooter({ text: `${TOURNAMENT_NAME}` })
+        .setTimestamp();
+
+    await channel.send({ embeds: [resultEmbed] });
+    await interaction.reply({ content: `Match ${data.matchId} submitted successfully!` });
+    await Promise.all([forfeitProfile, winnerProfile].map(emoji => emoji.delete()));
+
+}
+
+module.exports = { data, execute };
+export { };
